b757899a5af836089ffbc8d0c49bffb841f8129d
Alejandro Lopez <alzcol@outlook.com>
2022-05-21 23:06:04 -0400

    added tasks to left panel

--- lib/panel_left/panel_left_page.dart
+++ lib/panel_left/panel_left_page.dart
@@ -1,16 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:projectcrm/constants.dart';
+import 'package:projectcrm/responsive_layout.dart';
 
-class PanelLeftPage extends StatefulWidget{
-   @override
+class Todo {
+  String name;
+  bool enable;
+  Todo({this.enable = true, required this.name});
+}
+
+class PanelLeftPage extends StatefulWidget {
+  @override
   _PanelLeftPageState createState() => _PanelLeftPageState();
 }
 
-class _PanelLeftPageState extends State<PanelLeftPage>{
+class _PanelLeftPageState extends State<PanelLeftPage> {
+  List<Todo> _toDo = [
+    Todo(name: "Purchase Paper", enable: true),
+    Todo(name: "Refill the inventory of speakers", enable: true),
+    Todo(name: "Hire someone", enable: true),
+    Todo(name: "Marketing Strategy", enable: true),
+    Todo(name: "Selling furniture", enable: true),
+    Todo(name: "Finish the disclosure", enable: true),
+  ];
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
+      body: Stack(
+        children: [
+          if (ResponsiveLayout.isComputer(context))
+            Container(
+              color: Constants.purpleLight,
+              width: 50,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Constants.purpleDark,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                  ),
+                ),
+              ),
+            ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: Constants.kPadding / 2,
+                    right: Constants.kPadding / 2,
+                    top: Constants.kPadding / 2,
+                  ),
+                  child: Card(
+                    color: Constants.purpleLight,
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Container(
+                      width: double.infinity,
+                      child: ListTile(
+                        title: Text(
+                          "Products Sold",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        subtitle: Text(
+                          "18% of Products Sold",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        trailing: Chip(
+                          label: Text(
+                            "4,500",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: Constants.kPadding / 2,
+                    right: Constants.kPadding / 2,
+                    top: Constants.kPadding / 2,
+                    bottom: Constants.kPadding,
+                  ),
+                  child: Card(
+                    color: Constants.purpleLight,
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Column(
+                      children: List.generate(
+                        _toDo.length,
+                        (index) => CheckboxListTile(
+                          title: Text(
+                            _toDo[index].name,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          value: _toDo[index].enable,
+                          onChanged: (newValue) {
+                            setState(() {
+                              _toDo[index].enable = newValue ?? true;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
