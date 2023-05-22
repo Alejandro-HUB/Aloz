1ffff81feb014f6b9c75376680903eaef05eeae1
Alejandro Lopez <58063303+Alejandro-HUB@users.noreply.github.com>
2023-05-22 01:42:42 -0400

    Added search box functionality

--- lib/Pages/Home/app_bar_widget.dart
+++ lib/Pages/Home/app_bar_widget.dart
@@ -1,11 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:projectcrm/Pages/Users/profile_page.dart';
 import 'package:projectcrm/Helpers/Routing/route.dart';
 import 'package:projectcrm/main.dart';
 import 'package:projectcrm/Helpers/Constants/responsive_layout.dart';
 import '../../Helpers/Constants/Styling.dart';
 import '../../Helpers/Firebase/storage_service.dart';
+import '../Widgets/API/HttpRequestWidget.dart';
+import '../Widgets/Data/DataWidget.dart';
+import '../Widgets/GraphWidgets/panel_center_page.dart';
+import '../Widgets/GraphWidgets/panel_left_page.dart';
+import '../Widgets/GraphWidgets/panel_right_page.dart';
+import 'drawer_page.dart';
 
 List<String> _buttonNames = ["Overview", "Revenue", "Sales", "Control"];
 List<String> _menuItems = ["My Profile", "Logout"];
@@ -22,11 +31,38 @@ class AppBarWidget extends StatefulWidget {
 }
 
 class _AppBarWidgetState extends State<AppBarWidget> {
+  final TextEditingController _iconController = TextEditingController();
+  late List<WidgetsInfo> _widgetNames = [];
+
+  @override
+  void dispose() {
+    _iconController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     FirebaseAuth.instance.currentUser == null
         ? email = "user"
         : email = FirebaseAuth.instance.currentUser!.email!;
+
+    Widget createDynamicWidget(String widgetName) {
+      final Map<String, Widget Function()> widgetMap = {
+        'PanelLeftPage': () => PanelLeftPage(),
+        'PanelCenterPage': () => PanelCenterPage(),
+        'PanelRightPage': () => PanelRightPage(),
+        'DataWidget': () => const DataWidget(),
+        'HttpRequestWidget': () => const HttpRequestWidget(),
+        // Add more widget mappings here
+      };
+
+      if (widgetMap.containsKey(widgetName)) {
+        return widgetMap[widgetName]!();
+      } else {
+        // Return a fallback widget or handle the case when the widget name is not found
+        return Container();
+      }
+    }
 
     return Container(
       color: Styling.purpleLight,
@@ -137,11 +173,115 @@ class _AppBarWidgetState extends State<AppBarWidget> {
             ),
           const Spacer(),
           if (!ResponsiveLayout.isPhone(context))
-            IconButton(
-              color: Colors.white,
-              iconSize: 30,
-              onPressed: () {},
-              icon: const Icon(Icons.search),
+            //Search Button
+            Expanded(
+              child: TypeAheadFormField<Map<String, String>>(
+                textFieldConfiguration: TextFieldConfiguration(
+                  controller: TextEditingController(),
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    prefixIconColor: Colors.white,
+                    labelText: 'Search',
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                ),
+                suggestionsCallback: (pattern) async {
+                  // Fetch data from the collection and return suggestions based on the pattern
+                  const collectionName = "UserWidgets";
+                  final uid = FirebaseAuth.instance.currentUser!.uid;
+                  final querySnapshot = await FirebaseFirestore.instance
+                      .collection(collectionName)
+                      .doc(uid)
+                      .collection("UserWidgets:$uid")
+                      .get();
+
+                  for (final doc in querySnapshot.docs) {
+                    final id = doc.get('id') as String;
+                    final title = doc.get('title') as String;
+                    final iconName = doc.get('icon') as String;
+                    final icon = IconsExtension.getIconData(iconName);
+                    final widgetType = doc.get('widgetType') as String;
+                    final documentIdData = doc.get('documentIdData') as String;
+                    _widgetNames.add(WidgetsInfo(
+                        id: id,
+                        title: title,
+                        icon: icon,
+                        widgetType: widgetType,
+                        currentPage: 0,
+                        documentIdData: documentIdData));
+                  }
+
+                  // Extract suggestions from the querySnapshot and return them
+                  // Replace 'fieldName' with the actual field name in your collection documents
+                  return querySnapshot.docs
+                      .map((doc) => {
+                            'title': doc.get('title') as String,
+                            'widgetType': doc.get('widgetType') as String,
+                          })
+                      .where((suggestion) => suggestion['title']!
+                          .toLowerCase()
+                          .contains(pattern.toLowerCase()))
+                      .toList();
+                },
+                itemBuilder: (context, suggestion) {
+                  final title = suggestion['title']!;
+                  final widgetType = suggestion['widgetType']!;
+
+                  return ListTile(
+                    title: Text(
+                      title,
+                      style: const TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      'Type: $widgetType',
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  );
+                },
+                onSuggestionSelected: (Map<String, String> value) {
+                  // Perform actions when a suggestion is selected
+                  // Replace '_iconController' with the actual controller for the search field
+                  setState(() {
+                    _iconController.text =
+                        '${value['title']},${value['widgetType']}';
+
+                    //Parse widget properties
+                    var title = _iconController.text.split(',')[0];
+                    var type = _iconController.text.split(',')[1];
+
+                    //Get widget
+                    var widgetSelected = _widgetNames.firstWhere((element) =>
+                        element.title == title && element.widgetType == type);
+                    currentWidget = widgetSelected;
+                    currentWidget!.currentPage =
+                        _widgetNames.indexOf(widgetSelected) + 1;
+
+                    //Navigate to selected widget
+                    Navigator.of(context).push(MaterialPageRoute(
+                        // ignore: prefer_const_constructors
+                        builder: (context) => RoutePage(
+                              appBar: const AppBarWidget(),
+                              page: createDynamicWidget(
+                                  currentWidget!.widgetType),
+                              showDrawer: true,
+                            )));
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a search query';
+                  }
+                  return null;
+                },
+              ),
             ),
           if (!ResponsiveLayout.isPhone(context))
             Stack(
@@ -194,7 +334,7 @@ class _AppBarWidgetState extends State<AppBarWidget> {
                 ));
               } else if (index == "My Profile") {
                 Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => RoutePage(
+                    builder: (context) => const RoutePage(
                           appBar: AppBarWidget(),
                           page: ProfilePage(),
                           showDrawer: false,
